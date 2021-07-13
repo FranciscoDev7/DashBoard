864e8c6018e4c0d78fffca47a9725fb9815ba509
FranciscoDev7 <86272503+FranciscoDev7@users.noreply.github.com>
2021-07-13 20:29:28 -0300

    FIRST COMMIT

---
+++ textBook.ts
@@ -0,0 +1,26 @@
+/* 1. OS tipos de variavel TypeScript sao:
+string
+number
+boolean
+*/
+
+const myNameText:string = "Junior"
+let lastNumber:number = 25
+let isCorrect:boolean = true
+
+const fruits: string[] = []
+
+fruits.push("banana")
+
+const drinks: Array<string> = ["beer", "vodka"]
+
+interface IPerson{
+    name: string
+    age:number
+}
+
+const student: IPerson ={
+    name:"Jeff",
+    age:35
+}
+
