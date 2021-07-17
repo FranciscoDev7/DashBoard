c73108556774ffe4dbb2765d9695dd168f07600a
FranciscoDev7 <engmec.francisco@gmail.com>
2021-07-17 18:15:18 -0300

    Aside Style

--- my-wallet/src/components/Aside/index.tsx
+++ my-wallet/src/components/Aside/index.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
-import {Container} from './styles'
+import {Container, Title,Header, LogoImg, MenuContainer,MenuItemLink} from './styles'
+import Logo from '../../assests/logo.svg'
+import {MdDashboard, MdArrowDownward, MdArrowUpward, MdExitToApp} from 'react-icons/md'
 
 const Aside: React.FC = () => {
     return (
     <Container>
-        <h1>Aside</h1>
+        <Header>
+            <LogoImg src={Logo} alt= "Logo"/>
+            <Title>Minha Carteira</Title>
+        </Header>
+        <MenuContainer>
+           <MenuItemLink href='#'>
+            <MdDashboard/>
+                DashBoard
+            </MenuItemLink>
+           
+            <MenuItemLink href='#'>
+            <MdArrowUpward/>
+                Entradas
+            </MenuItemLink>
+            
+            <MenuItemLink href='#'>
+            <MdArrowDownward/>
+                Saídas
+            </MenuItemLink>
+            
+            <MenuItemLink href='#'>
+            <MdExitToApp/>
+                Sair
+            </MenuItemLink>
+        </MenuContainer>
     </Container>  
     )
 }
--- my-wallet/src/components/Aside/styles.ts
+++ my-wallet/src/components/Aside/styles.ts
@@ -1,7 +1,46 @@
 import styled from 'styled-components'
 
+
 export const Container = styled.div`
     grid-area: AS;
+    padding-left:20px;
     background-color: ${props=>props.theme.color.secondary};
+    
+    border-right: 1px solid ${props=>props.theme.color.gray};
+`
+export const Title = styled.h3`
     color:${props=>props.theme.color.white};
+    margin-left:10px;
+`
+export const Header = styled.header`
+    height: 70px;
+    display:flex;
+    align-items:center ;
+`
+
+export const LogoImg = styled.img`
+    height:40px;
+`
+
+export const MenuContainer=styled.nav`
+    display:flex;
+    flex-direction:column;  
+    margin-top:50px;
+`
+
+export const MenuItemLink=styled.a`
+    display:flex;
+    align-items:center; 
+    text-decoration:none;
+    color:${props=>props.theme.color.info};
+    transition:opacity .3s;
+    margin:7px 0; 
+    &:hover{
+        opacity:.7;
+
+    }
+    > svg{
+        margin-right:5px;
+        font-size:20px;
+    }
 `
